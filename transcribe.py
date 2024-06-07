cf2c04ea00602ac7d09a43809acd8959aeda4ad2
edgarbc <edgar.bermudez@gmail.com>
2024-06-06 20:51:41 -0600

    feat: transcribe.py added

---
+++ transcribe.py
@@ -0,0 +1,11 @@
+import whisper
+import sys
+
+def transcribe(file_path):
+    model = whisper.load_model("base")
+    result = model.transcribe(file_path)
+    return result["text"]
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    print(transcribe(file_path))
