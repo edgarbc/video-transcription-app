90752d0a3e1d7eb5c5162566484bb09f02514200
edgarbc <edgar.bermudez@gmail.com>
2024-06-09 14:33:17 -0600

    feat: requirements.txt added

--- transcribe.py
+++ transcribe.py
@@ -30,8 +30,13 @@ def extract_audio_from_video(video_path, audio_output_path):
 
 def transcribe(file_path):
     model = whisper.load_model("base")
+
+    print("Transcribing...")
+    print(model)
+
     result = model.transcribe(file_path)
     return result["text"]
+
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
